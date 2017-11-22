4de7a93d4967bc35368ce9ed9602cdc6806e2f02
TianlunGe <ge1tianlun@163.com>
2017-11-22 10:42:01 +0800

    l

--- src/leet/easy/Leet326PowerOfThree.java
+++ src/leet/easy/Leet326PowerOfThree.java
@@ -1,6 +1,11 @@
 package leet.easy;
 
 public class Leet326PowerOfThree {
+	
+	public static void main(String[] args){
+		System.out.println("qwe");
+	}
+	
 	public boolean isPowerOfThree(int n) {
         if(n < 1){
         	return false;
